ba7a9ff5612bef5cf3d1d7511a2d073353af5643
softwarespot <softwarespotapps@gmail.com>
2015-09-13 09:49:54 +0300

    Added version updating and jshint styling

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,11 @@ var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
+var replace = require('gulp-replace');
 var uglify = require('gulp-uglify');
 var del = require('del');
+var fs = require('fs');
+var merge = require('merge2');
 
 // See the uglify documentation for more details
 var uglifySettings = {
@@ -60,10 +63,45 @@ gulp.task('watch', function () {
     gulp.watch('./' + Assets.main, ['jshint', 'uglify']);
 });
 
+// Update version numbers based on the main file version comment
+gulp.task('version', function () {
+    var reVersion = /\n\s*\*\s+Version:\s+((?:\d+\.){2}\d+)/;
+    var version = fs.readFileSync('./' + Assets.main, {
+            encoding: 'utf8'
+        })
+        // Match is found in the 2nd element
+        .match(reVersion)[1];
+
+    var streams = merge();
+
+    // Main file VERSION constant
+    streams.add(
+        gulp.src('./' + Assets.main)
+        .pipe(replace(/VERSION\s+=\s+'(?:\d+\.){2}\d+';/, 'VERSION = \'' + version + '\';'))
+        .pipe(gulp.dest('.'))
+    );
+
+    // package.json version property
+    streams.add(
+        gulp.src('./package.json')
+        .pipe(replace(/"version":\s+"(?:\d+\.){2}\d+",/, '"version": "' + version + '",'))
+        .pipe(gulp.dest('.'))
+    );
+
+    // README.md version number
+    streams.add(
+        gulp.src('./README.md')
+        .pipe(replace(/^#\s+(\w+)\s+-\s+v(?:\d+\.){2}\d+/, '# $1 - v' + version))
+        .pipe(gulp.dest('.'))
+    );
+
+    return streams;
+});
 // Register the default task
-gulp.task('default', ['jshint', 'uglify']);
+gulp.task('default', ['version', 'jshint', 'uglify']);
 
 // 'gulp es6to5' to transpile from ES2015 to ES5
 // 'gulp jshint' to check the syntax
 // 'gulp uglify' to uglify the main file
 // 'gulp watch' to watch for changes to the main file
+// 'gulp version' to update the version numbers based on the main file version comment
