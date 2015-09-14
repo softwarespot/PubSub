8b8f62a259d48f9192036ee00083d2abcbfee004
softwarespot <softwarespotapps@gmail.com>
2015-09-14 08:20:27 +0300

    Moved comment

--- gulpfile.js
+++ gulpfile.js
@@ -65,6 +65,8 @@ gulp.task('watch', function () {
 
 // Update version numbers based on the main file version comment
 gulp.task('version', function () {
+    // SemVer matching is done using (?:\d+\.){2}\d+
+
     var reVersion = /\n\s*\*\s+Version:\s+((?:\d+\.){2}\d+)/;
     var version = fs.readFileSync('./' + Assets.main, {
             encoding: 'utf8'
@@ -73,8 +75,6 @@ gulp.task('version', function () {
         .match(reVersion)[1];
 
     var streams = merge();
-
-    // SemVer matching is done using (?:\d+\.){2}\d+
 
     // Main file VERSION constant
     streams.add(
